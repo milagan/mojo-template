1abd81b7261c9d6473df250b7a8af22aeeb36685
Maurice Ilagan <maurice.ilagan@checkpt.com>
2019-05-26 21:12:43 -0400

    New method for setting up routes.

--- lib/MojoTemplate.pm
+++ lib/MojoTemplate.pm
@@ -19,12 +19,26 @@ sub startup {
   $self->helper(log => sub { return $log; });
 
   # Router
-  my $r = $self->routes;
+  setup_routes($self->routes);
+}
 
-  # Normal route to controller
+sub setup_routes {
+  my $r = shift;
+
+  setup_example_route($r);
+  setup_data_route($r);
+}
+
+sub setup_example_route {
+  my $r = shift;
+
   $r->get('/')->to('example#welcome');
   $r->get('/welcome')->to('example#welcome');
   $r->get('/hello')->to('example#hello');
+}
+
+sub setup_data_route {
+  my $r = shift;
 
   $r->get('/api/data')->to('data#get');
   $r->post('/api/data')->to('data#post');
